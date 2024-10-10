cea6f3d97b15de906be23503b322b33b7beb3743
AlvaroViamont <alvaro.viamont.seraphys@gmail.com>
2024-10-10 11:37:44 -0400

    issue correction calculate ifg model

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,1 @@
-def calculate_ifg(creatine, age, gender, stature, weight):
-    # Fórmula para calcular IFG
-    if gender == 'M':
-        return (140 - age) * weight / (72 * creatine)
-    else:
-        return (140 - age) * weight / (85 * creatine)
+
